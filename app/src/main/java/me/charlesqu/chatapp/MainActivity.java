3f4feabaa4add503b968533d9998610d9e328324
charlesqu22 <charlesqu22@gmail.com>
2017-08-24 19:16:20 -0700

    first rfyh

--- app/src/main/java/me/charlesqu/chatapp/MainActivity.java
+++ app/src/main/java/me/charlesqu/chatapp/MainActivity.java
@@ -1,13 +1,16 @@
 package me.charlesqu.chatapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
 
     @Override
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void enterChat(View view){
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+
+
+
+
+
+
+
+
+    }
+
 
 }
