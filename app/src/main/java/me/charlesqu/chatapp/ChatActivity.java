b70c45cbdb38c1b9802149528daa4b3df28ba49b
lockshaw <mastakata3@yahoo.com>
2017-12-31 15:26:00 -0800

    Fix importing issue

--- app/src/main/java/me/charlesqu/chatapp/ChatActivity.java
+++ app/src/main/java/me/charlesqu/chatapp/ChatActivity.java
@@ -11,7 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-git import android.view.View;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
